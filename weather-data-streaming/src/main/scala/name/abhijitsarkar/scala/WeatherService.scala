aafd4cafc8dd39ced6c8f187fac29e8479dfe1cb
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2016-08-23 01:02:36 -0700

    Small improvements

--- weather-data-streaming/src/main/scala/name/abhijitsarkar/scala/WeatherService.scala
+++ weather-data-streaming/src/main/scala/name/abhijitsarkar/scala/WeatherService.scala
@@ -1,19 +1,20 @@
 package name.abhijitsarkar.scala
 
-import java.nio.file.{ DirectoryStream, Files, Path, Paths }
+import java.io.File
+import java.nio.file.{DirectoryStream, Files, Path, Paths}
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.FileIO.fromPath
-import akka.stream.scaladsl.{ Flow, Keep, Merge, Sink, Source }
+import akka.stream.scaladsl.{Flow, Keep, Merge, Sink, Source}
 import akka.util.ByteString
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.util.Try
 
 /**
- * @author Abhijit Sarkar
- */
+  * @author Abhijit Sarkar
+  */
 object WeatherService {
 
   import scala.collection.JavaConverters._
@@ -23,19 +24,19 @@ object WeatherService {
   }).iterator.asScala.toList
 
   def combine(a: List[Path]): Source[ByteString, Any] = a match {
-    case Nil            => Source.empty
-    case i :: Nil       => fromPath(i)
-    case i :: j :: Nil  => Source.combine(fromPath(i), fromPath(j))(Merge(_))
+    case Nil => Source.empty
+    case i :: Nil => fromPath(i)
+    case i :: j :: Nil => Source.combine(fromPath(i), fromPath(j))(Merge(_))
     case i :: j :: tail => Source.combine(fromPath(i), fromPath(j), combine(tail))(Merge(_))
   }
 
   import ElementType._
 
-  val dailyWeathers = Flow[ByteString]
+  def dailyWeathers(implicit ec :ExecutionContext) = Flow[ByteString]
     .mapConcat(_.utf8String.split("\\R").toStream)
-    .map(_.split(",").map(_.trim))
+    .mapAsyncUnordered(4)(x => Future(x.split(",").map(_.trim)))
     .filter(_.size >= 4)
-    .map { arr =>
+    .mapAsyncUnordered(4) { arr =>
       val stationId = StationId(arr(0))
 
       val dateOfObservation = DateOfObservation(arr(1))
@@ -52,45 +53,46 @@ object WeatherService {
 
       val timeOfObservation = arr.lift(7).map(TimeOfObservation(_))
 
-      DailyWeather(stationId, dateOfObservation, elementType, elementValue,
-        measurementFlag, qualityFlag, sourceFlag, timeOfObservation)
+      Future(DailyWeather(stationId, dateOfObservation, elementType, elementValue,
+        measurementFlag, qualityFlag, sourceFlag, timeOfObservation))
     }
 
-  import name.abhijitsarkar.scala.Downloader._
+  import Downloader._
 
   val summary = Flow[DailyWeather]
-    .filter(x => x.elementType.code == maxTemp.code && x.dateOfObservation.isDefined)
+    .filter(x => x.elementType == maxTemp && x.dateOfObservation.isDefined)
     .groupBy(numYears, _.dateOfObservation.map(_.year).get)
-    .filter(_.elementType.code == maxTemp.code)
     .fold((0, 0.0f)) {
       (acc, w) => (w.dateOfObservation.map(_.year).getOrElse(-1), Try(acc._2.max(w.elementValue.toFloat)).getOrElse(0.0f))
     }
     .mergeSubstreams
 
-  def run(inDir: String) =
-    combine(inFiles(inDir)).via(dailyWeathers).via(summary).toMat(
-      Sink.foreach(x => println(s"Year: ${x._1}, Max temp: ${x._2}."))
-    )(Keep.right) // Keeps the future
-
   def main(args: Array[String]): Unit = {
-    import Downloader._
-    import WeatherConstants._
-
-    import scala.concurrent.duration._
-
     implicit val system = ActorSystem("weather")
     implicit val materializer = ActorMaterializer()
+    // for use with Futures, Scheduler, etc.
+    import system.dispatcher
+
+    import WeatherConstants._
 
     val dir = inDir
 
     val downloadResult = downloadAndExtract(dir).run()
 
+    import scala.concurrent.duration._
     Await.result(downloadResult, 2.minutes)
 
-    val finalResult = WeatherService.run(dir).run()
+    def run(inDir: String) =
+      combine(inFiles(inDir)).via(dailyWeathers).via(summary).toMat(
+        Sink.foreach(x => println(s"Year: ${x._1}, Max temp: ${x._2}."))
+      )(Keep.right) // Keeps the future
+
+    val finalResult = run(dir).run()
 
     Await.result(finalResult, 10.minutes)
 
-    val maybeTerminate = system.terminate()
+    system.terminate()
+
+    new File(dir).delete
   }
 }
